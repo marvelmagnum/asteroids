590441ba0886556230f9d5b0234dc01701dc2554
marvel_magnum <marvel_magnum@yahoo.com>
2024-08-23 00:50:19 -0300

    added movement and collision logic

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -11,14 +15,41 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = {updateable, drawable}
+    Asteroid.containers = {updateable, drawable, asteroids}
+    AsteroidField.containers = {updateable}
     
+    asteroid_field = AsteroidField()
+    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0,0,0))
+
+        for item in updateable:
+            item.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(ship):
+                print("Game over!")
+                pygame.quit()
+                sys.exit()
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0   # in secs
 
 if __name__ == "__main__":
     main()
